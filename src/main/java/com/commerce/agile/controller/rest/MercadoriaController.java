bfe7ee1a004dc181afe5789168b6a94c3e72ca19
f0nt_CODE <font3002@outlook.com>
2026-05-15 16:09:09 -0300

    [FASE: 1] Listagem de produtos criada

--- src/main/java/com/commerce/agile/controller/rest/MercadoriaController.java
+++ src/main/java/com/commerce/agile/controller/rest/MercadoriaController.java
@@ -1,15 +1,30 @@
 package com.commerce.agile.controller.rest;
 
+import com.commerce.agile.dto.mercadoria.ResponseMercadoriaDTO;
+import com.commerce.agile.service.MercadoriaService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/mercadoria")
 public class MercadoriaController {
 
+    @Autowired
+    private MercadoriaService mercadoriaService;
+
     @GetMapping
-    public String upMainPage(){
+    public String upMainPage(Model model){
+
+        List<ResponseMercadoriaDTO> mercadorias = mercadoriaService.listarTodas();
+
+        model.addAttribute("mercadorias", mercadorias);
+
         return "ProdutoList";
     }
 
