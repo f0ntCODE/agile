77b3a5d9fa22ffa09691f61865853867220a7b0d
f0nt_CODE <font3002@outlook.com>
2026-06-01 20:45:06 -0300

    [FASE: 2] JWT é carregado pleo Spring via dependência 'dotenv'

--- src/main/java/com/commerce/agile/AgileApplication.java
+++ src/main/java/com/commerce/agile/AgileApplication.java
@@ -1,5 +1,6 @@
 package com.commerce.agile;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -7,8 +8,6 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 public class AgileApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AgileApplication.class, args);
-	}
+	public static void main(String[] args) {SpringApplication.run(AgileApplication.class, args);}
 
 }
